e03ede9bd5ed07c3022b22e7ed90a9a274884e73
Joshua Park <git@joshprk.me>
2025-12-30 23:52:17 -0500

    refactor: remove clippy dead code warning

--- crates/vtui-macros/src/lib.rs
+++ crates/vtui-macros/src/lib.rs
@@ -3,7 +3,7 @@ use quote::quote;
 use syn::{ItemFn, parse_macro_input};
 
 #[proc_macro_attribute]
-pub fn component(attr: TokenStream, item: TokenStream) -> TokenStream {
+pub fn component(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let func = parse_macro_input!(item as ItemFn);
 
     let attrs = func.attrs;
