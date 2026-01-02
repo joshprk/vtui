2f2a774e81f0f5fd265a61ccba8b00c08d84b6bc
Joshua Park <git@joshprk.me>
2026-01-02 00:25:29 -0500

    fix: sync simple_app example to new changes

--- examples/simple_app.rs
+++ examples/simple_app.rs
@@ -2,14 +2,12 @@ use ratatui::style::Style;
 use vtui::prelude::*;
 
 #[component]
-fn App(c: &mut Component) -> Inner {
+fn App(c: &mut Component) {
     c.draw(move |ctx| {
         let style = Style::default();
         let text = "Hello world";
         ctx.buf.set_string(1, 1, text, style);
     });
-
-    Inner::default()
 }
 
 fn main() -> anyhow::Result<()> {
