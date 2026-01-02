5ed31ed264ba2144924f85c976fda576631f0bf0
Joshua Park <git@joshprk.me>
2026-01-02 05:27:39 -0500

    feat: implement useful driver methods

--- crates/vtui-core/src/driver.rs
+++ crates/vtui-core/src/driver.rs
@@ -1,9 +1,11 @@
-use std::sync::mpsc::Sender;
+use std::io;
 
-use ratatui::prelude::Backend;
-
-use crate::events::Message;
+use ratatui::{Terminal, prelude::Backend};
 
 pub trait Driver {
     type Backend: Backend;
+
+    fn setup(&mut self) -> io::Result<()>;
+    fn teardown(self) -> io::Result<()>;
+    fn terminal(&mut self) -> &mut Terminal<Self::Backend>;
 }
