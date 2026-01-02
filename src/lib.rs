b548356d09a2c48f6587687e4ec1b6e05fca6bce
Joshua Park <git@joshprk.me>
2026-01-02 01:51:05 -0500

    chore: run formatter

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,9 @@ pub use vtui_core::events;
 pub use vtui_core::runtime;
 
 pub mod prelude {
-    pub use vtui_core::{driver::Driver, component::Component};
+    pub use vtui_core::{component::Component, driver::Driver};
     pub use vtui_macros::component;
 }
 
-#[cfg(not(any(
-    feature = "crossterm",
-)))]
-compile_error!(
-    "vtui requires a terminal driver. Enable one of: crossterm"
-);
+#[cfg(not(any(feature = "crossterm")))]
+compile_error!("vtui requires a terminal driver. Enable one of: crossterm");
