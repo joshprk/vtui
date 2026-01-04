44c3c8c430d8095f35d94752e9d0da6bdb6b7226
Joshua Park <git@joshprk.me>
2026-01-04 18:40:06 -0500

    feat: add counter to show event source working

--- examples/simple_app.rs
+++ examples/simple_app.rs
@@ -1,13 +1,20 @@
+use std::{cell::RefCell, rc::Rc};
+
 use ratatui::style::Style;
-use vtui::prelude::*;
+use vtui::{events::Tick, prelude::*};
 
 #[component]
 fn App(c: &mut Component) {
+    let counter = Rc::new(RefCell::new(0));
+    let set_counter = counter.clone();
+
     c.draw(move |ctx| {
         let style = Style::default();
-        let text = "Hello world";
+        let text = format!("Counter: {}", counter.borrow());
         ctx.buf.set_string(1, 1, text, style);
     });
+
+    c.listen::<Tick>(move |ctx| *set_counter.borrow_mut() += 1);
 }
 
 fn main() {
