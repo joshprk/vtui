dcf7fb75af9b3d46a3d10b30c09bfedf3038f784
Joshua Park <git@joshprk.me>
2026-01-12 08:56:47 -0500

    refactor: remove unnecessary mut in StateOwner::insert

--- crates/vtui-core/src/state.rs
+++ crates/vtui-core/src/state.rs
@@ -2,6 +2,7 @@ use std::cell::{Ref, RefMut};
 
 use generational_box::{AnyStorage, GenerationalBox, GenerationalRef, GenerationalRefMut, Owner, UnsyncStorage};
 
+#[derive(Default)]
 pub(crate) struct StateOwner {
     inner: Owner<UnsyncStorage>,
 }
@@ -12,7 +13,7 @@ impl StateOwner {
         Self { inner }
     }
 
-    pub fn insert<T: 'static>(&mut self, value: T) -> State<T> {
+    pub fn insert<T: 'static>(&self, value: T) -> State<T> {
         let inner = self.inner.insert(value);
         State { inner }
     }
