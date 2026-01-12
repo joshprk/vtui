d28480d55958b99222814873d01f5c3197f83702
Joshua Park <git@joshprk.me>
2026-01-12 09:13:52 -0500

    feat: improve simple_app and make it the README example

--- examples/simple_app.rs
+++ examples/simple_app.rs
@@ -1,27 +1,23 @@
 use ratatui::style::Style;
-use vtui::{
-    events::{KeyPress, MouseDown},
-    input::{KeyCode, MouseButton},
-    prelude::*,
-};
+use vtui::{events::*, input::*, prelude::*};
 
 #[component]
 fn App(c: &mut Component) -> Inner {
-    let mut counter = c.state(0);
+    let mut clicks = c.state(0);
 
     c.draw(move |canvas| {
-        let text = format!("Counter: {}", counter.read());
+        let text = format!("Clicks: {}", *clicks.read());
         canvas.text(0, 0, text, Style::default());
     });
 
     c.listen::<MouseDown>(move |event| {
         if event.button == MouseButton::Left {
-            *counter.write() += 1;
+            *clicks.write() += 1;
         }
     });
 
     c.listen::<KeyPress>(|event| {
-        if let KeyCode::Char('q') = event.key {
+        if event.key == KeyCode::Char('q') {
             event.request_shutdown();
         }
     });
