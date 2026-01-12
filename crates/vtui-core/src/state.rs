a9651b348019dfbb98d85eb7e9540c6fde0f28e2
Joshua Park <git@joshprk.me>
2026-01-12 09:27:18 -0500

    refactor: remove unused State::new

--- crates/vtui-core/src/state.rs
+++ crates/vtui-core/src/state.rs
@@ -1,7 +1,7 @@
 use std::cell::{Ref, RefMut};
 
 use generational_box::{
-    AnyStorage, GenerationalBox, GenerationalRef, GenerationalRefMut, Owner, UnsyncStorage,
+    GenerationalBox, GenerationalRef, GenerationalRefMut, Owner, UnsyncStorage,
 };
 
 #[derive(Default)]
@@ -10,11 +10,6 @@ pub(crate) struct StateOwner {
 }
 
 impl StateOwner {
-    pub fn new() -> Self {
-        let inner = UnsyncStorage::owner();
-        Self { inner }
-    }
-
     pub fn insert<T: 'static>(&self, value: T) -> State<T> {
         let inner = self.inner.insert(value);
         State { inner }
