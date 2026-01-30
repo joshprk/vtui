7c573a0d336aef6521e34be7725f9f9756aad2d8
Joshua Park <git@joshprk.me>
2026-01-30 15:58:10 -0500

    feat: add LaunchBuilder

--- src/launch.rs
+++ src/launch.rs
@@ -8,26 +8,46 @@ use crate::{
     transport::MessageBus,
 };
 
-pub fn launch(app: Factory) -> Result<(), RuntimeError> {
-    let node = Node::from(app);
-    let bus = MessageBus::new();
-    let mut driver = CrosstermDriver::new(io::stdout())?;
+#[derive(Default)]
+pub struct LaunchBuilder {}
 
-    driver.setup()?;
-    driver.spawn_event_handler(bus.sender());
-
-    let mut runtime = Runtime::new(node, bus);
-
-    loop {
-        runtime.draw(&mut driver)?;
-        runtime.update();
-
-        if runtime.should_exit() {
-            break;
-        }
+impl LaunchBuilder {
+    pub fn new() -> Self {
+        LaunchBuilder::default()
     }
 
-    driver.teardown()?;
+    pub fn launch(self, app: Factory) -> Result<(), RuntimeError> {
+        let node = Node::from(app);
 
-    Ok(())
+        let bus = MessageBus::new();
+        let mut driver = CrosstermDriver::new(io::stdout())?;
+
+        driver.setup()?;
+        driver.spawn_event_handler(bus.sender());
+
+        let mut runtime = Runtime::new(node, bus);
+
+        loop {
+            runtime.draw(&mut driver)?;
+            runtime.update();
+
+            if runtime.should_exit() {
+                break;
+            }
+        }
+
+        driver.teardown()?;
+
+        Ok(())
+    }
+}
+
+/// Launches an app with the given root component.
+///
+/// # Panics
+///
+/// This function is allowed to panic if the event loop errors. If you want to handle panics in a
+/// controlled manner, use the [`LaunchBuilder`] manually.
+pub fn launch(app: Factory) {
+    LaunchBuilder::new().launch(app).expect("app panicked unexpectedly");
 }
